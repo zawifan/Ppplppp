20184dba128806421d13317b801764296a345082
Thomas Rytz <zawifan@users.noreply.github.com>
2014-08-26 10:01:34 +0200

    hello Git world! github 2014-08-26 1

--- HelloGitWorld/src/HelloGitWorld.java
+++ HelloGitWorld/src/HelloGitWorld.java
@@ -7,5 +7,6 @@ public class HelloGitWorld {
 	public static void main(String[] args) {
 		System.out.println("hello Git world!");
 		System.out.println("hello Git world! lokal");
+		System.out.println("hello Git world! github 2014-08-26 1");
 	}
 }
