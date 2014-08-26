82f0d82db0a58577d164392f4f4baadc190e0801
Thomas Rytz <thomas_rytz.2008@bluewin.ch>
2014-08-26 16:15:44 +0200

    eclipse hellooGitWorld 12

--- HelloGitWorld/src/HelloGitWorld.java
+++ HelloGitWorld/src/HelloGitWorld.java
@@ -9,6 +9,6 @@ public class HelloGitWorld {
 		System.out.println("hello Git world! lokal");
 		System.out.println("hello Git world! gitbash 2014-08-26 4");
 		System.out.println("hello Git world! github 2014-08-26 1");
-		System.out.println("hello Git world! eclipse 2014-08-26 11");
+		System.out.println("hello Git world! eclipse 2014-08-26 12");
 	}
 }
