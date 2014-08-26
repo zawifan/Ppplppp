1435cc8cfcf57799af0f9d2236ec4bcb4d858a16
Thomas Rytz <thomas_rytz.2008@bluewin.ch>
2014-08-26 13:51:39 +0200

    gitbash hellogitWorld 2

--- HelloGitWorld/src/HelloGitWorld.java
+++ HelloGitWorld/src/HelloGitWorld.java
@@ -7,7 +7,7 @@ public class HelloGitWorld {
 	public static void main(String[] args) {
 		System.out.println("hello Git world!");
 		System.out.println("hello Git world! lokal");
-		System.out.println("hello Git world! gitbash 2014-08-26 1");
+		System.out.println("hello Git world! gitbash 2014-08-26 2");
 		System.out.println("hello Git world! github 2014-08-26 1");
 		System.out.println("hello Git world! eclipse 2014-08-26 9");
 	}
