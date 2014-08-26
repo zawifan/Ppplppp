3e05283a5e28acef447bf52ed21cabb990a329d4
Thomas Rytz <thomas_rytz.2008@bluewin.ch>
2014-08-26 13:27:47 +0200

    aus gitbash Main.java

--- HelloGitWorld/src/Main.java
+++ HelloGitWorld/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello main lokal 2014-08-26 2");
+		System.out.println("hello main eclipse 2014-08-26 2");
+		System.out.println("hello main gitbash 2014-08-26 1");
 	}
 }
