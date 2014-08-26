3c5434489aa325c5969f99e67c64cf208e766a32
Thomas Rytz <thomas_rytz.2008@bluewin.ch>
2014-08-26 13:29:16 +0200

    commit from eclipse 5

--- HelloGitWorld/src/Main.java
+++ HelloGitWorld/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello main lokal 2014-08-26 2");
+		System.out.println("hello main eclipse 2014-08-26 5");
 	}
 }
