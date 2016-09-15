eff277ed450a72de04229310355be981663b3c3c
Thomas Rytz <thomas.rytz@born.ch>
2016-09-15 14:31:52 +0200

    Main.java

--- HelloGitWorld/src/Main.java
+++ HelloGitWorld/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) { 
 		System.out.println("hello main eclipse 2014-08-26 5");
 		System.out.println("hello main gitbash 2014-08-26 1");
+		System.out.println("hello main gitbash 2016-09-15");
 	}
 }
