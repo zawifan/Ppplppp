3e8c23731987472db3ff19903cec33656425366e
Thomas Rytz <zawifan@users.noreply.github.com>
2016-10-18 10:14:12 +0200

    Update Main.java

--- HelloGitWorld/src/Main.java
+++ HelloGitWorld/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("hello main gitbash 2016-09-15");
 		System.out.println("hello main gitbash 2016-10-18");
 		System.out.println("hello main gitbash 2016-10-18b");
+		System.out.println("hello main gitbash 2016-10-18c");
 	}
 }
