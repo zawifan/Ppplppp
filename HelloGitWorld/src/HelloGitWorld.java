5b1b6fa9f00d3eafa26dbed2465afdacd3d45833
Thomas Rytz <zawifan@users.noreply.github.com>
2016-10-26 12:11:10 +0200

    Update HelloGitWorld.java

--- HelloGitWorld/src/HelloGitWorld.java
+++ HelloGitWorld/src/HelloGitWorld.java
@@ -21,5 +21,6 @@ public class HelloGitWorld {
 		System.out.println("hello Git world! eclipse 2016-10-18h github+eclipse");
 		System.out.println("hello Git world! eclipse 2016-10-18i eclipse");
 		System.out.println("hello Git world! eclipse 2016-10-18i github");
+		System.out.println("hello Git world! eclipse 2016-10-26j github");
 	}
 }
